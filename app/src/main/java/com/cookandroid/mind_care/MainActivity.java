4f35624856a6d456c44aff275210f65bfc403017
ll0zlk <hyojin041025@gmail.com>
2024-12-03 12:51:02 +0900

    feat: 산책 기능 구현

--- app/src/main/java/com/cookandroid/mind_care/MainActivity.java
+++ app/src/main/java/com/cookandroid/mind_care/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         walkingBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, WalkingActivity.class);
+                Intent intent = new Intent(MainActivity.this, com.cookandroid.mind_care.WalkingActivity.class);
                 startActivity(intent);
             }
         });
